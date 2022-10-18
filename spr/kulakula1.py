7dff6a55bc00cb85d8ece06b686a85100f8fd961
j4ck0b <75251166+j4ck0b@users.noreply.github.com>
2022-10-18 22:07:38 +0200

    Add files via upload

---
+++ spr/kulakula1.py
@@ -0,0 +1,9 @@
+from math import pi
+def szescian():
+    a = float(input("podaj a: "))
+    return 6 * a**2
+def kula():
+    r = float(input("podaj r: "))
+    return 4 * pi * r**2
+print(szescian() - kula())
+
